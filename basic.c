321cd9c84fce51f37b01084369aa5595cafdda48
nidhi-nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-01-21 21:36:51 +0530

    new code

--- basic.c
+++ basic.c
@@ -1,8 +1,22 @@
-#include <stdio.h>
-#include <conio.h>
-int main()
+#include<stdio.h>
+main()
 {
-    printf("Nidhi Nupur\n iamnidhinupur@gmail.com\n CSE\n 7324905503\n");
-    getch();
-    return 0;
+    int a=5,b=7,c;
+    int *p,*q;
+    // p=&a;
+    q=(&b,&a);
+    // c=*q;
+    printf("Enter the value of a=%d\n",a);
+    printf("Enter the value of b=%d\n",b);
+    // printf("Enter the value of a=%d\n",*p);
+    printf("Address of b=%d\n",&b);
+    printf("The value of q=%d\n",q);
+    // printf("Address of a=%x\n",p);
+    // printf("Address of a=%x\n",&a);
+    // printf("Address of p=%d\n",&p);
+    // printf("Address of p=%x\n",&p);
+    // printf("Address of q=%d\n",&q);
+    // printf("Address of =%x\n",&q);
+    // printf("Enter the value of c=%d\n",c);
+    // printf("Address of c=%x\n",&c);
 }
