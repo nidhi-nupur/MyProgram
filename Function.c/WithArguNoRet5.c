b17dc2697c4c875bfcc5ecf21cd31485338d23d4
nidhi-nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-02-15 18:46:32 +0530

    Added one code

---
+++ Function.c/WithArguNoRet5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void sum(float,float); 
+void sum(float x,float y)
+{
+    printf("Sum=%f\n",x+y);
+}
+void main()
+{
+    float x,y;
+    printf("x=");
+    scanf("%f",&x);
+    printf("y=");
+    scanf("%f",&y);
+    sum(x,y);
+}
