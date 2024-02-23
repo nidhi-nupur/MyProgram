a9b86adb2ac2baaab8ecbca752a16ddcde58803e
nidhi-nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-02-23 17:37:22 +0530

    Add one code

---
+++ Structure_Basic_9.c
@@ -0,0 +1,13 @@
+// 1st way to declare a Structure variable.
+// 2nd Code to find the size of structure datatype.
+#include<stdio.h>
+struct student
+{
+    int roll_no;
+    float marks;
+    char name[30];
+};
+void main()
+{
+    printf("Structure size=%d",sizeof(struct student));
+}
