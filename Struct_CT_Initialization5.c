0b8cb31a239ba808e2c0cc8296530f8351a3964f
nidhi-nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-02-27 18:10:52 +0530

    Add one code

---
+++ Struct_CT_Initialization5.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+struct student
+{
+    int roll_no;
+    char name[30];
+    float marks;
+}S1={1,"Nidhi",90.65};
+ S2={2,"Nupur",95.85}; // This will considered as wrong cause we can't declare two variable by separating it with semi-colon.
