b5fd0f475a3fef3ee104c4f6a7fcd6507a262928
nidhi-nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-03-02 17:57:10 +0530

    Add one code

---
+++ Struct_CT_Initialization16.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+struct student
+{
+    int roll_no;
+    char name[30];
+    float marks;
+};
+void main()
+{
+    struct student s1={1,"Nidhi",97.8};
+    struct student s2;
+    s2=s1;
+    printf("Information of s1:\n");
+    printf("%d\t%s\t%f\n",s1.roll_no,s1.name,s1.marks);
+    printf("Information of s2:\n");
+    printf("%d\t%s\t%f\n",s2.roll_no,s2.name,s2.marks);
+}
