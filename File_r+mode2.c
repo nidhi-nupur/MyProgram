5da38b4eae958f2c81719914aa30e702ffb45567
nidhi-nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-03-20 15:36:56 +0530

    Add one code

---
+++ File_r+mode2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    FILE *fp=NULL;
+    fp=fopen("First.txt","r+"); 
+    if(fp==NULL)
+    {
+        printf("Error");
+        exit(1);
+    }
+    fputs("This is Nidhi Nupur",fp);
+    fclose(fp);
+}
