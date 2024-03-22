f41071149b6242cd9266076c3e27491e8821bce7
nidhi-nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-03-22 20:12:30 +0530

    Add one code

---
+++ File_Read_5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void main()
+{
+    FILE *fp=NULL;
+    char str[50];
+    fp=fopen("First.txt","r");
+    if(fp==NULL)
+    {
+        printf("Error");
+        exit(1);
+    }
+    while(!feof(fp))
+    {
+        fgets(str,5,fp);
+        printf("%s",str);
+    }
+    fclose(fp);
+}
