de312f47d5372e7e97114bb6b104780d83894af5
nidhi-nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-03-25 19:28:12 +0530

    Add one code

---
+++ fseek()_w_1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    FILE *fp=NULL;
+    char c;
+    fp=fopen("First.txt","w");
+    if(fp==NULL)
+    {
+        printf("File doesn't exist.");
+        exit(1);
+    }
+    fseek(fp,5,SEEK_SET);
+    c=fgetc(fp);
+    printf("%c\n",c);
+    fseek(fp,-2,SEEK_CUR);
+    c=fgetc(fp);
+    printf("%c\n",c);
+    fseek(fp,-6,SEEK_END);
+    c=fgetc(fp);
+    printf("%c\n",c);
+}
